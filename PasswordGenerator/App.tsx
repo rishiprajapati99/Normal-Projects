79d9fc4022932c562d10613b26fc05b65179c753
rishiprajapati99 <santoshprajapati52911@gmail.com>
2025-08-30 10:10:37 +0530

    Provided the yup validations and replaced the useState from Formik for better results by yup {[^.^]}

--- PasswordGenerator/App.tsx
+++ PasswordGenerator/App.tsx
@@ -1,114 +1,135 @@
+import React from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
-import { useState } from 'react';
-import * as yup from 'yup';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import { TextInput } from 'react-native-paper';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 
 export default function PasswordGenerator() {
-  const PasswordSchema = yup.object().shape({
-    passwordLength: yup
-      .number()
-      .min(6, 'minimum 6 characters are required')
-      .max(16, 'maximum 16 characters are allowded')
-      .required('length is required'),
-  });
-  const [passwordLength, setPasswordLength] = useState();
-  const [lowercase, isLowercase] = useState(false);
-  const [uppercase, isUppercase] = useState(false);
-  const [numbers, isNumbers] = useState(false);
-  const [symbols, isSymbols] = useState(false);
   return (
     <SafeAreaProvider>
       <SafeAreaView style={[styles.mainSafeAreaView]}>
-        <View style={[styles.mainView]}>
-          <View style={[styles.headingTextContainer]}>
-            <Text style={[styles.headingText]}>Password Generator</Text>
-          </View>
-          <View
-            style={[
-              styles.InputContainerBox,
-              // styles.border
-            ]}
-          >
-            <View
-              style={[
-                styles.inputContainer,
-                // styles.border
-              ]}
-            >
-              <Text style={styles.inputHeading}>Password Length</Text>
-              <TextInput
-                placeholder="Ex. 8"
-                label="Length"
-                style={{ width: '20%' }}
-                keyboardType={'numeric'}
-                value={passwordLength}
-                onChangeText={() => setPasswordLength}
-              />
-            </View>
-            <Text>{passwordLength}</Text>
-            <View style={[styles.inputContainer]}>
-              <Text style={[styles.inputHeading]}>
-                Include Lowercase Letters
-              </Text>
-              <BouncyCheckbox
-                style={[styles.radioButton]}
-                fillColor="#2ecc71"
-                onPress={() => {
-                  isLowercase(!lowercase);
-                }}
-              />
-            </View>
-            <View style={[styles.inputContainer]}>
-              <Text style={[styles.inputHeading]}>
-                Include Upperrcase Letters
-              </Text>
-              <BouncyCheckbox
-                style={[styles.radioButton]}
-                fillColor="#2ecc71"
-                onPress={() => isUppercase(!uppercase)}
-              />
-            </View>
-            <View style={[styles.inputContainer]}>
-              <Text style={[styles.inputHeading]}>Include Numbers</Text>
-              <BouncyCheckbox
-                style={[styles.radioButton]}
-                fillColor="#2ecc71"
-                onPress={() => isNumbers(!numbers)}
-              />
-            </View>
-            <View style={[styles.inputContainer]}>
-              <Text style={[styles.inputHeading]}>Include Symbols</Text>
-              <BouncyCheckbox
-                style={[styles.radioButton]}
-                fillColor="#2ecc71"
-                onPress={() => isSymbols(!symbols)}
-              />
-            </View>
-            <View style={[styles.buttonsContainer]}>
-              <TouchableOpacity
-                style={[styles.buttons, { backgroundColor: '#4885caff' }]}
-              >
-                <Text
-                  style={[styles.inputHeading, styles.buttonHeadings]}
-                  onPress={() => {
-                    Alert.alert('Length', passwordLength);
-                  }}
+        <Formik
+          initialValues={{
+            //all the initial state of the values needs to be written inside initialvalues just like in the useState()
+            passwordLength: '',
+            isLowerCase: false,
+            isUpperCase: false,
+            isNumbers: false,
+            isSymbols: false,
+          }}
+          validationSchema={yup.object({
+            //all the yup validations are needs to be written inside the validationSchema
+            passwordLength: yup
+              .number()
+              .min(6, 'minimum 6 characters are required')
+              .max(16, 'maximum 16 characters are allowded')
+              .required('Password length is required'),
+          })}
+          onSubmit={values => {
+            Alert.alert('Password Length', JSON.stringify(values));
+          }}
+        >
+          {({ values, setFieldValue, handleSubmit }) => {
+            //setFieldValue function set the current state of the value just like setValue in useState()
+            return (
+              <View style={[styles.mainView]}>
+                <View style={[styles.headingTextContainer]}>
+                  <Text style={[styles.headingText]}>Password Generator</Text>
+                </View>
+                <View
+                  style={[
+                    styles.InputContainerBox,
+                    // styles.border
+                  ]}
                 >
-                  Generate
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.buttons, { backgroundColor: '#e3ecf1e7' }]}
-              >
-                <Text style={[styles.inputHeading, styles.buttonHeadings]}>
-                  Reset
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
+                  <View
+                    style={[
+                      styles.inputContainer,
+                      // styles.border
+                    ]}
+                  >
+                    <Text style={styles.inputHeading}>Password Length</Text>
+                    <TextInput
+                      placeholder="Ex. 8"
+                      label="Length"
+                      style={{ width: '20%' }}
+                      keyboardType={'numeric'}
+                      value={values.passwordLength}
+                      onChangeText={txt => {
+                        setFieldValue('passwordLength', txt); //setFieldValue requires atLeast 2 arguments . 1)key(the most imp , it must be exactly same as the initialName we have given in the initalValues . this function recognise by this key that  which state has to be updated by this value) 2)value
+                      }}
+                    />
+                  </View>
+                  <View style={[styles.inputContainer]}>
+                    <Text style={[styles.inputHeading]}>
+                      Include Lowercase Letters
+                    </Text>
+                    <BouncyCheckbox
+                      style={[styles.radioButton]}
+                      fillColor="#2ecc71"
+                      onPress={txt => {
+                        setFieldValue('isLowerCase', txt);
+                      }}
+                    />
+                  </View>
+                  <Text>{values.isLowerCase ? 'true' : 'false'}</Text>
+                  <View style={[styles.inputContainer]}>
+                    <Text style={[styles.inputHeading]}>
+                      Include Upperrcase Letters
+                    </Text>
+                    <BouncyCheckbox
+                      style={[styles.radioButton]}
+                      fillColor="#2ecc71"
+                      onPress={txt => setFieldValue('isUpperCase', txt)}
+                    />
+                  </View>
+                  <Text>{values.isUpperCase ? 'true' : 'false'}</Text>
+                  <View style={[styles.inputContainer]}>
+                    <Text style={[styles.inputHeading]}>Include Numbers</Text>
+                    <BouncyCheckbox
+                      style={[styles.radioButton]}
+                      fillColor="#2ecc71"
+                      onPress={txt => setFieldValue('isNumbers', txt)}
+                    />
+                  </View>
+                  <Text>{values.isNumbers ? 'true' : 'false'}</Text>
+                  <View style={[styles.inputContainer]}>
+                    <Text style={[styles.inputHeading]}>Include Symbols</Text>
+                    <BouncyCheckbox
+                      style={[styles.radioButton]}
+                      fillColor="#2ecc71"
+                      onPress={txt => setFieldValue('isSymbols', txt)}
+                    />
+                  </View>
+                  <Text>{values.isSymbols ? 'true' : 'false'}</Text>
+                  <View style={[styles.buttonsContainer]}>
+                    <TouchableOpacity
+                      style={[styles.buttons, { backgroundColor: '#4885caff' }]}
+                      onPress={handleSubmit}
+                    >
+                      <Text
+                        style={[styles.inputHeading, styles.buttonHeadings]}
+                      >
+                        Generate
+                      </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={[styles.buttons, { backgroundColor: '#e3ecf1e7' }]}
+                    >
+                      <Text
+                        style={[styles.inputHeading, styles.buttonHeadings]}
+                      >
+                        Reset
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            );
+          }}
+        </Formik>
       </SafeAreaView>
     </SafeAreaProvider>
   );
